d7df61c96fba2448227c7e4ef37ab7e2025034d1
Jaime Cervantes <jaime.cervantes.ve@gmail.com>
2022-12-03 19:10:02 -0600

    styles(tsx): change logout colors

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -5,7 +5,7 @@ export default function LogoutButton() {
     <Form action="/logout" method="post">
       <button
         type="submit"
-        className="rounded bg-slate-600 py-2 px-4 text-white hover:bg-blue-800 active:bg-blue-900"
+        className="rounded bg-gradient-to-tr from-slate-900 to-transparent py-2 px-4 text-white hover:bg-blue-800 active:bg-blue-900"
       >
         Cerrar sesión
       </button>
